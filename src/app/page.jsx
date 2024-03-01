c2f77ba3808d8af4ddfc6a31f835a3e9b83b0055
Beasty786 <shameelnkosi@gmail.com>
2024-03-01 19:45:25 +0200

    going to start with framer motion

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,25 @@
+import Image from "next/image";
+
 const Homepage = () => {
-  return <div></div>;
+  return <div className=" h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+    {/* IMAGE CONTAINER */}
+    <div className=" h-1/2 lg:h-full lg:w-1/2  relative">
+      <Image src="/hero.png" alt="" fill className="object-contain"/>
+    </div>
+
+    {/* TEXT CONTAINER */}
+    <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+      {/* TITLE */}
+      <h1 className="text-4xl md:text-6xl font-bold">Crafting Digital Experience, Designing Tomorrow.</h1>
+      {/* Description */}
+      <p className="md:text-xl"> welcome to my degital canvas, where innovation and creativity converge.</p>
+      {/* Buttons */}
+      <div className="flex gap-4 w-full">
+        <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">View My Work</button>
+        <button className="p-4 rounded-lg ring-1  ring-black">Contact me</button>
+      </div>
+    </div>
+  </div>;
 };
 
 export default Homepage;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useState } from "react";
+import NavLink from "./navLink";
 
 const links = [
     { url: "/", title: "Home"},
@@ -16,16 +17,16 @@ const Navbar = () => {
     const [open,SetOpen] = useState(false)
     return (
         
-        <div className="h-full flex items-center justify-between px- sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
             {/* HORIZONTAL MENU */}
             <div className="hidden md:flex gap-4 w-1/3">
                 {links.map(link => (
-                    <Link href={link.url} key={link.title}> {link.title}</Link>
+                    <NavLink link={link} key={link.title}></NavLink>
                 ))}
             </div>
 
             {/* LOGO */}
-            <div className="md:hidden lg:flex w-1/3 justify-center">
+            <div className="md:hidden lg:flex xl:w-1/3 xl:justify-center">
                 <Link href="/" className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center">
                     <span className="text-white mr-1">
                         GOTE
@@ -56,9 +57,9 @@ const Navbar = () => {
                 {/* Menu Button */}
                 <button className="w-10 h-8 flex  flex-col justify-between z-50 relative" 
                         onClick={() => SetOpen(!open)}>
-                    <div className="w-10 h-1 bg-white rounded"></div>
-                    <div className="w-10 h-1 bg-white rounded"></div>
-                    <div className="w-10 h-1 bg-white rounded"></div>
+                    <div className="w-10 h-1 bg-black rounded"></div>
+                    <div className="w-10 h-1 bg-black rounded"></div>
+                    <div className="w-10 h-1 bg-black rounded"></div>
                 </button>
                 {/* MENU LIST */}
 
